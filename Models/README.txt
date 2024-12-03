34d3fa4f64104cb867f2412d635f74abdf88d1a0
Will Buchta <jwbuchta@wpi.edu>
2024-12-03 00:35:57 -0500

    Clean working directory

--- Models/README.txt
+++ Models/README.txt
@@ -1,1 +1,12 @@
 Put PyTorch here
+
+Merged Genres:
+genres, (ids)
+Electronic, Ambient Electronic, techno (15, 42, 181)
+Rock, Indie-Rock, Psych-Rock
+Hip-hop
+Punk
+Folk
+Instrumental
+Pop, experimental pop
+Classical
---
+++ Models/data/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=64g
+#SBATCH -J "FULL_create_tensors"
+#SBATCH -p short
+#SBATCH -t 4:00:00
+#SBATCH --mail-user=jwbuchta@wpi.edu
+#SBATCH --mail-type=BEGIN,FAIL,END
+
+module purge
+module load slurm #cuda12.1 #python/3.12.4
+
+now=$(date)
+echo "INFO [run.sh] Starting execution on $now"
+
+#source /home/jwbuchta/CS539_Project/Autoencoder/venv_autoencoder/bin/activate
+#which $HOME/CS539_Project/Autoencoder/venv_autoencoder/bin/python
+$HOME/CS539_Project/Autoencoder/venv_autoencoder/bin/python get_song_y.py
+
+#sleep 600
+
+now=$(date)
+echo "INFO [run.sh] Finished execution at $now"
